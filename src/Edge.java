35be137ca8e7b7f8093f0280d67c6c2e3c6a7071
smokingwolf31 <132780282+smokingwolf31@users.noreply.github.com>
2023-06-04 00:59:55 +0200

    Update Edge.java
    Documantation completed...

--- src/Edge.java
+++ src/Edge.java
@@ -4,6 +4,15 @@ class Edge
     public Vertex     dest;   // Second vertex in Edge
     public double     cost;   // Edge cost
     
+    /*
+    * Only used during testing 
+    */
+    private Edge(){}
+    /**
+    * Represents an edge formed by two vertexes 
+    *@param d pass in the vertex you which to go to
+    *@param c pass in the cost of the edge
+    */
     public Edge( Vertex d, double c )
     {
         dest = d;
